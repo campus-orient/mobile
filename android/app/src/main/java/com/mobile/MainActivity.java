a63d5f8c2cd52ed3967a62dfcbbc86d5124d04c9
ProgrammerJayT <theanthem8@gmail.com>
2024-04-28 21:14:07 +0200

    Add JAVA bundle

--- android/app/src/main/java/com/mobile/MainActivity.java
+++ android/app/src/main/java/com/mobile/MainActivity.java
@@ -1,14 +1,22 @@
 package com.mobile;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
+   * 
    */
   @Override
   protected String getMainComponentName() {
@@ -16,8 +24,10 @@ public class MainActivity extends ReactActivity {
   }
 
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
+   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView
+   * is created and
    * you can specify the rendered you wish to use (Fabric or the older renderer).
+   * 
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
