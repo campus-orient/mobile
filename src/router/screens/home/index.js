4a3d155016ead1e49da34bc0a058755d0dba9a03
ProgrammerJayT <theanthem8@gmail.com>
2024-05-08 15:51:16 +0200

    Create home screen

---
+++ src/router/screens/home/index.js
@@ -0,0 +1,18 @@
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={styles.root}>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  root: {
+    //
+  },
+});
